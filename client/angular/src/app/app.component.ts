1804462f1b423638797d4b4180f2882fb3a22fd0
Rohan Drummond <rohan@Rohans-Laptop.local>
2024-12-11 16:05:50 +0700

    added http functionality and enabled cors

--- client/angular/src/app/app.component.ts
+++ client/angular/src/app/app.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common'; 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
+
 export class AppComponent {
-  title = 'angular';
+  data: string = '';
+  constructor(private http : HttpClient) {};
+  fetchData() {
+    const apiUrl: string = 'http://localhost:5095/helloworld';
+    this.http.get(apiUrl, { responseType: 'text' }).subscribe({
+      next: (response) => {
+        this.data = response;
+        console.log('Data fetched successfully: ', this.data);
+      },
+      error: (e) => {
+        console.error('Error fetching data: ', e)
+      }
+    })
+  }
 }
--- server/dotnet/Program.cs
+++ server/dotnet/Program.cs
@@ -1,5 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngularApp", policy =>
+    {
+        policy.WithOrigins("http://localhost:4200")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
+
+app.UseCors("AllowAngularApp");
+
 app.UseHttpsRedirection();
 
 app.MapGet("/helloworld", () => 
