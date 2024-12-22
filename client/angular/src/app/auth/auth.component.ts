8a66025a7deb3439840f3e287755cfe49a652d90
Rohan Drummond <drummond.rohan@gmail.com>
2024-12-22 14:10:50 +0700

    implemented rxjs for authentication state management

--- client/angular/src/app/auth/auth.component.ts
+++ client/angular/src/app/auth/auth.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { LoginFormComponent } from '../login-form/login-form.component';
 import { RegisterFormComponent } from '../register-form/register-form.component';
 import { SupabaseService } from '../supabase.service';
+import { Session } from '@supabase/supabase-js';
 
 @Component({
   selector: 'app-auth',
@@ -15,11 +16,17 @@ import { SupabaseService } from '../supabase.service';
   styleUrl: './auth.component.css'
 })
 export class AuthComponent {
+
   constructor(private readonly supabase: SupabaseService) {}
+
   formTemplate: string = "login";
+
+  session: Session | null = null;
+
   applyLogin = () => {
     this.formTemplate = "login"
   }
+
   applyRegister = () => {
     this.formTemplate = "register"
   }
@@ -38,18 +45,9 @@ export class AuthComponent {
     }
   }
 
-  async getUser(): Promise<void> {
-    try {
-      console.log("Get user being triggered.")
-      const { data, error } = await this.supabase.getUser();
-      if (error) throw error
-      console.log(data)
-    } catch (error) {
-      if (error instanceof Error) {
-        alert(error.message)
-      }
-    } finally {
-      console.log("User data fetched successfully.")
-    }
-  }
+  async getSession(): Promise<void> {
+    this.supabase.sessionObservable.subscribe((session) => {
+      this.session = session;
+    });
+  };
 }
--- client/angular/src/app/supabase.service.ts
+++ client/angular/src/app/supabase.service.ts
@@ -1,16 +1,31 @@
 import { Injectable } from '@angular/core'
-import { createClient, SupabaseClient } from '@supabase/supabase-js'
 import { environment } from '../environments/environment'
+import { BehaviorSubject ,Observable } from 'rxjs'
+import { createClient, SupabaseClient, Session, AuthChangeEvent } from '@supabase/supabase-js'
 
 @Injectable({ providedIn: 'root' })
 
 export class SupabaseService {
   
-  private supabase: SupabaseClient
+  private supabase: SupabaseClient;
+  
+  private sessionSubject: BehaviorSubject<Session | null> = new BehaviorSubject<Session | null>(null);
+  public sessionObservable: Observable<Session | null> = this.sessionSubject.asObservable();
 
   constructor() {
     this.supabase = createClient(environment.supabaseUrl, environment.supabaseKey)
-  }
+    this.listenToAuthChanges();
+  };
+
+  private listenToAuthChanges(): void {
+    this.supabase.auth.onAuthStateChange((event: AuthChangeEvent, session: Session | null) => {
+      if (event === 'SIGNED_IN' || event === 'TOKEN_REFRESHED' || event === 'INITIAL_SESSION') {
+        this.sessionSubject.next(session);
+      } else if (event === 'SIGNED_OUT') {
+        this.sessionSubject.next(null);
+      }
+    });
+  };
 
   signUp = (username: string, email: string, password: string) => {
     return this.supabase.auth.signUp({
@@ -21,19 +36,15 @@ export class SupabaseService {
           username
         }
       }
-    })
-  }
+    });
+  };
 
   signIn = (email: string, password: string) => {
     return this.supabase.auth.signInWithPassword({ email, password});
-  }
+  };
 
   signOut = () => {
     return this.supabase.auth.signOut();
-  }
+  };
 
-  getUser = () => {
-    return this.supabase.auth.getUser();
-  }
-
-}
+};
